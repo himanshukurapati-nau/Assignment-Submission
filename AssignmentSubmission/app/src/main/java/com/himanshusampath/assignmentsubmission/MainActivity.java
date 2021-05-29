9c4749ced9a5b67ec103219a62a82d362c1eeb39
Himanshu <64647720+AstroGamers-0217-2020@users.noreply.github.com>
2021-05-29 22:25:00 +0530

    First commit

---
+++ AssignmentSubmission/app/src/main/java/com/himanshusampath/assignmentsubmission/MainActivity.java
@@ -0,0 +1,36 @@
+package com.himanshusampath.assignmentsubmission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginbutton;
+    private Button registerbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginbutton = findViewById(R.id.login_button);
+        registerbutton = findViewById(R.id.register_button);
+        
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    private void openLoginActivity(){
+        Intent intent=new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
